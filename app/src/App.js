709dcc5bda7dc47186d24ee598ad7ce2d208d1d7
Cinguinique Erquette <erquette@ualberta.ca>
2023-01-07 17:37:59 -0700

    Converter UI v.1.0

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
+//import "./styles.css";
+import Converter from "./components/inputField"
+import Dropdown from "./components/Dropdown"
 
-function App() {}
-
-export default App;
+export default function App(){
+    const options = [
+        {value: "opt1", label: "Option 1"},
+        {value: "opt2", label: "Option 2"}
+    ];
+    return(
+        <div className = "App">
+            <Dropdown placeHolder="Select..." options={options}/>
+            <br></br>
+            <Converter></Converter>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Dropdown placeHolder="Select..." options={options}/>
+            <br></br>
+            <Converter></Converter>
+        </div>
+    );
+}
---
+++ app/src/components/inputField.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import "./inputField.css";
+function Field() {
+  const [data, setData]=useState(null)
+  function getData(val){
+    setData(val.target.value)
+    console.log(val.target.value)
+  }
+  return (
+        <div className='input-field'>
+          <input type="text" onChange={getData} />
+        </div>
+        //{/* <div className='input-field'> Result</div> */}
+  )
+}
+
+export default Field

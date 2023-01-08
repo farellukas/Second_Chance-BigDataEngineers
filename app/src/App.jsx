a3acdd6e5eccf4e0c8ff8e7b33bb679419587dfc
Randy luna <randyluna@Randys-MacBook-Air.local>
2023-01-08 10:05:48 -0700

    translate with styling

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import Createiframe from "./pages/Createiframe";
 import Navbar from "./Navbar"
 import Translator from "./pages/Translator"
 import Home from "./pages/Home"
+import Comments from "./components/Comments"
 import About from "./pages/About"
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
@@ -15,7 +16,7 @@ function App() {
         <Routes>
             {/* //<Route path='/Articles' element={<Articles/>}/> */}
             <Route path='/Createiframe' element={<Createiframe/>}/>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Comments />} />
             <Route path="/Articles" element={<Articles />} />
             <Route path="/Translator" element={<Translator />} />
         </Routes>
